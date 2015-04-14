7baa74efd92049c2c31a0bbcc340ea9a760aea6c
restjohn <restjohn@users.noreply.github.com>
2015-04-14 10:44:07 -0600

    metromap:
    - removing functions from scope that aren't used outside the app controller
    - added some code cleanup bad user point data on load
    - set a default description on user points when the description is empty so the marker popup and thus the delete button is still available
    - set an id on the user point properties so the delete function can key off of that instead of the description text

--- metromap/js/app.js
+++ metromap/js/app.js
@@ -3,6 +3,15 @@
 var parismetromap = angular.module("parismetromap", []);
 
 parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) {
+  var generateUuid = function() {
+    var d = new Date().getTime();
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+  };
+
   $scope.locations = [];
   $scope.newLocation = {};
   $scope.showControls = false;
@@ -17,14 +26,36 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
 
 
   // load the points from local storage
-  $scope.loadPoints = function() {
-    if ($scope.supportsLocalStorage() && localStorage["dice.parismetromap.locations"]) {
+  var loadPoints = function() {
+    if (localStorageSupported() && localStorage["dice.parismetromap.locations"]) {
       $scope.locations = JSON.parse(localStorage["dice.parismetromap.locations"]);
       if (!$scope.locations) {
         $scope.locations = {
           "type": "FeatureCollection",
           "features": []
         };
+      }
+      else {
+        // clean up bad data
+        var count = $scope.locations.features.length;
+        while (count--) {
+          var feature = $scope.locations.features[count];
+          if (!feature.geometry) {
+            $scope.locations.features.splice(count, 1);
+          }
+          else {
+            if (!feature.properties) {
+              feature.properties = {};
+            }
+            if (!feature.properties.id) {
+              feature.properties.id = generateUuid();
+            }
+            if (!feature.properties.text) {
+              feature.properties.text = 'Point of interest';
+            }
+          }
+        }
+        storeUserPoints();
       }
       console.log("Controller: loaded points: ", $scope.locations);
     } else {
@@ -34,16 +65,29 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
   };
 
 
+  var storeUserPoints = function() {
+    localStorage.setItem("dice.parismetromap.locations", JSON.stringify($scope.locations));
+  };
+
+
   // handle writing the point to local storage
   $scope.saveLocation = function() {
     console.log("Controller: saving location: ", $scope.newLocation);
-    if (!$scope.supportsLocalStorage()) { return false; }
+
+    if (!localStorageSupported()) {
+      return false;
+    }
+
+    if (!$scope.newLocation.properties.text) {
+      $scope.newLocation.properties.text = 'Point of interest';
+    }
     $scope.locations.features.push($scope.newLocation);
 
-    localStorage.setItem("dice.parismetromap.locations", JSON.stringify($scope.locations));
+    storeUserPoints();
 
     $scope.showControls = false;
     $scope.newLocation = {};
+
     return true;
   };
 
@@ -63,6 +107,7 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
         "coordinates": [$scope.center.lng, $scope.center.lat]
       },
       "properties": {
+        "id": generateUuid(),
         "text": ""
       }
     };
@@ -74,7 +119,8 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
     var index = -1;
 
     for (var i = 0; i < $scope.locations.features.length; i++) {
-      if ($scope.locations.features[i].properties.text == $scope.activeLocation._popup._content) {
+      var test = $scope.locations.features[i].properties.id || $scope.locations.features[i].properties.text;
+      if (test == $scope.activeLocation.feature.properties.id || test == $scope.activeLocation.feature.properties.text) {
         index = i;
       }
     }
@@ -89,7 +135,7 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
 
 
   // Check to see if you can use HTML local storage
-  $scope.supportsLocalStorage = function() {
+  var localStorageSupported = function() {
     try {
       return 'localStorage' in window && window['localStorage'] !== null;
     } catch (e) {
@@ -101,7 +147,7 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
   // Geolocation stuff
   $scope.getLocation = function() {
     console.log("Controller: in getLocaion");
-    $scope.connectWebViewJavascriptBridge(function(bridge) {
+    connectWebViewJavascriptBridge(function(bridge) {
       bridge.callHandler('getLocation', {}, function(response) {
         if (typeof response == "string") {
           console.log("converting string to json object");
@@ -110,7 +156,7 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
         console.log("Received callback from geolocation: " + response);
         console.log("Type of response: " + typeof response);
         if (response && String(response.success).toLowerCase() == 'true') {
-          $scope.setCoordinates(response);
+          setCoordinates(response);
           $scope.didGeolocate = true;
         } else { // no GPS? center on France
           console.log("No navigator, setting coordinates to defaults... " + response.message);
@@ -122,7 +168,7 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
   };
 
 
-  $scope.setCoordinates = function(position) {
+  var setCoordinates = function(position) {
     console.log("Controller: Attempting to set coortinates lat: " + position.lat + " lon: " + position.lon);
     console.log("set coordinates: ", $scope);
     $scope.$apply(function () {
@@ -136,7 +182,7 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
   $scope.exportPoints = function() {
     console.log("fixin' to export");
     $scope.statusText = "Exporting GeoJSON";
-    $scope.connectWebViewJavascriptBridge(function(bridge) {
+    connectWebViewJavascriptBridge(function(bridge) {
       bridge.callHandler('saveToFile', $scope.locations, function(response) {
         console.log("Received callback from data export: " + response);
         if (typeof response == "string") {
@@ -150,7 +196,7 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
   };
 
 
-  $scope.connectWebViewJavascriptBridge = function(callback) {
+  var connectWebViewJavascriptBridge = function(callback) {
     if (window.WebViewJavascriptBridge) {
       callback(WebViewJavascriptBridge);
     } else {
@@ -161,8 +207,8 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
   };
 
 
-  $scope.initBridge = function() {
-    $scope.connectWebViewJavascriptBridge(function(bridge) {
+  var initBridge = function() {
+    connectWebViewJavascriptBridge(function(bridge) {
       bridge.init(function(message, responseCallback) {
         console.log('JS got a message', message)
       })
@@ -179,6 +225,6 @@ parismetromap.controller('AppCtrl', function AppCtrl($rootScope, $scope, $http) 
     $scope.showInfo = !$scope.showInfo;
   };
 
-  $scope.loadPoints();
-  $scope.initBridge();
+  loadPoints();
+  initBridge();
 });
